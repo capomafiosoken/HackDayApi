5bc44be2fc80e1aa0a68aeee58d4a357f5530039
Galymzhan K <capomafiosoken@gmail.com>
2020-05-04 14:06:02 +0600

    c

--- Controllers/CameraAddressController.cs
+++ Controllers/CameraAddressController.cs
@@ -28,7 +28,30 @@ namespace HackDayApi.Controllers
             return Ok(a);
         }
 
-        [HttpPost]
+        [HttpPost("/camera")]
+        public async Task<IActionResult> UploadCameraAddress(List<IFormFile> files)
+        {
+            foreach (var file in files)
+            {
+                if (file.Length > 0)
+                {
+                    // await using var stream = System.IO.File.Create(file.FileName);
+                    // await file.CopyToAsync(stream);
+                    var a =file.OpenReadStream();
+                    var reader = ExcelReaderFactory.CreateReader(a).AsDataSet();
+                    foreach (DataTable dataTable in reader.Tables)
+                    {
+                        foreach (DataRow dataRow in dataTable.Rows)
+                        {
+                            Console.WriteLine(dataRow[0]);
+                        }
+                    }
+                }
+            }
+            return Ok(files.Count);
+        }
+        
+        [HttpPost("/client")]
         public async Task<IActionResult> PostFile(List<IFormFile> files)
         {
             foreach (var file in files)
