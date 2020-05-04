05c9e345e8ccf6a5b4b7915d309ddb473ace144e
aibekzh <aibek_zhumash@mail.ru>
2020-05-04 16:29:36 +0600

    asd

--- CameraAddressService.cs
+++ CameraAddressService.cs
@@ -61,5 +61,53 @@ namespace HackDayApi
             }
             return null;
         }
+        public async Task<List<House>> SaveClients(IFormFile file)
+        {
+            var a = file.OpenReadStream();
+            var reader = ExcelReaderFactory.CreateReader(a).AsDataSet();
+            foreach (DataTable dataTable in reader.Tables)
+            {
+                foreach (DataRow dataRow in dataTable.Rows)
+                {
+                    string address = dataRow[1].ToString();
+                    var house = await _context.Houses.FirstOrDefaultAsync(x => x.Address == address);
+                    if (house!=null)
+                    {
+                        house = new House
+                        {
+                            Address = address
+                        };
+                        var response = await Geocoder.GeocodeAddress(house.Address);
+                        house.Latitude = response.Data.Items[0].Coordinates[0];
+                        house.Longitude = response.Data.Items[0].Coordinates[1];
+                        var entrancesList = new List<Models.Entrance>();
+                        for (var i = 0; i < response.Data.Items[0].Entrances.Length; i++)
+                        {
+                            entrancesList.Add(new Models.Entrance
+                            {
+                                Number = i + 1,
+                                Latitude = response.Data.Items[0].Entrances[i].Coordinates[0],
+                                Longitude = response.Data.Items[0].Entrances[i].Coordinates[1]
+                            });
+                        }
+                        await _context.Houses.AddAsync(house);
+                        await _context.Entrances.AddRangeAsync(entrancesList);
+                        await _context.SaveChangesAsync();
+                    }
+                    var client = await _context.Entrances.FirstOrDefaultAsync(x=>x.Id == house.Id && x.Number == int.Parse(dataRow[1].ToString()));
+                    enter.CameraNumber = int.Parse(dataRow[2].ToString());
+                    await _context.SaveChangesAsync();
+                    var Client = new Client();
+                    Client.FullName = dataRow[0].ToString();
+                    //var address = dataRow[1].ToString();
+                    //var entrance_id = int.Parse(dataRow[2].ToString());
+                    Client.ApartmentNumber = int.Parse(dataRow[3].ToString());
+                    Client.PhoneNumber = dataRow[4].ToString();
+                    Client.TariffPlan = dataRow[5].ToString();
+                }
+            }
+
+            return null;
+        }
     }
 }
